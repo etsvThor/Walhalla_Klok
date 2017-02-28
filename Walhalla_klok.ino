5ef3d89b1446f07c7a8367c885abe5e0eb25df1a
Sebastiaan Goossens <seb@thor.edu>
2017-02-28 12:49:58 +0100

    Fix compiler warning

--- Walhalla_klok.ino
+++ Walhalla_klok.ino
@@ -312,7 +312,7 @@ time_t getNtpTime() {
 }
 
 // send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
+void sendNTPpacket(IPAddress& address)
 {
   // set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
